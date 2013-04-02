6eee7fceb026e08a498819c17882878b4de6f34b
juhan88 <jujuhan@gmail.com>
2013-04-01 22:05:30 -0700

    add schema.xml

--- query/simpleQuery.groovy
+++ query/simpleQuery.groovy
@@ -1,29 +1,30 @@
 
-@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.6' )
-import groovyx.net.http.*
-import static groovyx.net.http.ContentType.*
-import static groovyx.net.http.Method.*
+
+// import groovyx.net.http.*
+// import static groovyx.net.http.ContentType.*
+// import static groovyx.net.http.Method.*
 
 
 
 def simpleQuery(){
 	print "Enter Query >>"
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in))
-	def userInput = br.readLine()
+	// BufferedReader br = new BufferedReader(new InputStreamReader(System.in))
+	// def userInput = br.readLine()
 	// println "Query: $userInput"
 
 	
 	def defaultQueryString = "http://localhost:8983/solr/select?q="	
 
-	// def query = "country:India"
+	def userInput = "country:India"
 	def type = "&wt=json"
 
-	def http = new HTTPBuilder(defaultQueryString+userInput+type)
-	http.request(GET,JSON){
-		
+	@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.6' )
+	def http = new groovyx.net.http.HTTPBuilder(defaultQueryString+userInput+type)
+	http.request(GET,JSON){		
 		response.success = { resp, json ->
 			json.response.docs.each{
-				println it
+				println it.professor
+				println it.schools
 			}	 	
     
 		}
